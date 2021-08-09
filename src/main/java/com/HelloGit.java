c506ae701eac226c14a81746b92ff30f1a14a9f7
Peng <peng@qq.com>
2021-08-09 15:56:36 +0800

    my idea second commit

---
+++ src/main/java/com/HelloGit.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Hello world!
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit!");
+        System.out.println("HelloGit!2");
+    }
+}
